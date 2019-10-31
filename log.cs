8b47c312060fb257c91a6df85629b22ea8203d3e
Ahmed Elnably <aelnably@microsoft.com>
2019-10-31 15:01:54 -0700

    Add files via upload

---
+++ log.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ignite_logs
+{
+    public static class log
+    {
+        [FunctionName("log")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            Console.WriteLine("Trying Console.WriteLine");
+
+            bool shouldThrow = req.Query["throw"].Count > 0;
+            if (shouldThrow)
+            {
+                throw new InvalidOperationException("boom!");
+            }
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+            log.LogInformation("Name = " + (name!=null?name:""));
+
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
